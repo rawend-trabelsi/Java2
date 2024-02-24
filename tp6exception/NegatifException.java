c7a5eae1c9d41b8cc8fba949fb7dac353147fb7b
rawend-trabelsi <147106657+rawend-trabelsi@users.noreply.github.com>
2024-02-24 23:27:53 +0100

    Delete tp6exception directory

--- tp6exception/NegatifException.java
+++
@@ -1,7 +0,0 @@
-package tp6exception;
-
-public class NegatifException extends Exception {
-	NegatifException(String msg){
-	super(msg);
-
-}}
--- tp6exception/ParametreNullException.java
+++
@@ -1,8 +0,0 @@
-package tp6exception;
-
-public class ParametreNullException extends Exception {
-	
-	ParametreNullException(String msg){
-		super(msg);
-	}
-}
